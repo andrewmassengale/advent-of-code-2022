2c3f599a1f8887a498e5a41f75efbf73e8f6317b
Andrew Massengale <andrew.massengale@gmail.com>
2022-12-05 23:07:31 -0600

    day 6

---
+++ day6/part1.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileData = fs.readFileSync(path.join(__dirname, 'input.txt'));
+const data = fileData.toString().split('');
+
+let count = 0;
+let str = '';
+let startOfPacket = -1;
+
+data.forEach((char) => {
+	count = ++count;
+	if (str.length !== 4) {
+		return str += char;
+	}
+	str = str.substring(1) + char;
+	const uniqChars = new Set(str).size;
+	if (uniqChars === 4 && startOfPacket === -1) {
+		startOfPacket = count;
+	}
+});
+
+console.log(startOfPacket);
